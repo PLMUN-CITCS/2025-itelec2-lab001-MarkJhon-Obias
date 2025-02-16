a6aa81ae6f777fbd303ce03cd64aaa6f19ee210d
MarkJhon-Obias <obiasmarkjhon_bsit@plmun.edu.ph>
2025-02-16 19:11:18 +0800

    My first Python program

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-print("Hello world!")
-print("Name: Markjhon Obias")
-print("Course: BSIT")
-print("Interests in programming: Coding")
+def main():
+    # Your main code here
+    print("Hello, world!")
+    print("Name: Markjhon Obias")
+    print("Course: BSIT")
+    print("Interests in programming: Coding")
+
+if __name__ == "__main__":
+    main()
