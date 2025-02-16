c16bc2def66784831ff0a27e56c2cbc8bb0f7288
MarkJhon-Obias <obiasmarkjhon_bsit@plmun.edu.ph>
2025-02-16 20:36:27 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
-def main():
-    # Your main code here
-    print("Hello, world!")
-    print("Name: Markjhon Obias")
-    print("Course: BSIT")
-    print("Interests in programming: Coding")
+print("Hello, world!")
+print("Name: Markjhon Obias")
+print("Course: BSIT")
+print("Interests in programming: Coding")
 
-if __name__ == "__main__":
-    main()
